b5356860a3dca5050cbee69ca4ade4435ca30258
Sofia Rodriguez <snd998.sr@gmail.com>
2026-03-12 17:36:39 -0500

    functions

---
+++ functions.py
@@ -0,0 +1,6 @@
+calificaciones = [85, 92,78]
+promedio = float(sum(calificaciones) / len(calificaciones))
+resultado = promedio
+def promedio_estudiante(calificaciones, promedio):
+    return resultado(float) 
+print(resultado)
