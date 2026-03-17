2c06c613848c2d39a95851e79414ddf9216b5028
Sofia Rodriguez <snd998.sr@gmail.com>
2026-03-16 22:42:13 -0500

    33

--- multiple_files.py
+++ multiple_files.py
@@ -1,11 +1,11 @@
-from utils import flip, count_letters
+from utils import *
 
 def main():
     message = input("Please type your message\n")
 
-    reversed_message = flip(message)
-    a_count = count_letters(message, "a")
+    flipped = flip(message)
+    count = count_letters(message, "a")
 
-    encoded_message = reversed_message + str(a_count)
+    encoded = flipped + str(count)
 
-    print("Your encoded message is: " + encoded_message)
+    print("Your encoded message is: " + encoded)
