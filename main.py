11e4e5820cbad3baa5c11613e343bb24a111f5cc
Sofia Rodriguez <snd998.sr@gmail.com>
2026-03-16 21:51:11 -0500

    7

--- main.py
+++ main.py
@@ -9,46 +9,38 @@ while True:
     if operation == "exit":
         break
 
-    if operation not in [
-        "add",
-        "subtract",
-        "multiply",
-        "divide",
-        "exponent",
-        "modulo",
-        "floor_divide",
-        "absolute",
-    ]:
+    if operation not in ["add", "subtract", "multiply", "divide", "exponent", "modulo", "floor_divide", "absolute"]:
         print("Invalid option!")
         continue
 
     if operation == "absolute":
         num = float(input("Enter the number:\n"))
         result = absolute(num)
+        print(f"The result is: {result}")
+        continue
 
-    else:
-        num1 = float(input("Enter the first number:\n"))
-        num2 = float(input("Enter the second number:\n"))
+    num1 = float(input("Enter the first number:\n"))
+    num2 = float(input("Enter the second number:\n"))
 
-        if operation == "add":
-            result = add(num1, num2)
+    if operation == "add":
+        result = add(num1, num2)
 
-        elif operation == "subtract":
-            result = sub(num1, num2)
+    elif operation == "subtract":
+        result = sub(num1, num2)
 
-        elif operation == "multiply":
-            result = multiply(num1, num2)
+    elif operation == "multiply":
+        result = multiply(num1, num2)
 
-        elif operation == "divide":
-            result = divide(num1, num2)
+    elif operation == "divide":
+        result = divide(num1, num2)
 
-        elif operation == "exponent":
-            result = exponent(num1, num2)
+    elif operation == "exponent":
+        result = exponent(num1, num2)
 
-        elif operation == "modulo":
-            result = modulo(num1, num2)
+    elif operation == "modulo":
+        result = modulo(num1, num2)
 
-        elif operation == "floor_divide":
-            result = floor_divide(num1, num2)
+    elif operation == "floor_divide":
+        result = floor_divide(num1, num2)
 
     print(f"The result is: {result}")
